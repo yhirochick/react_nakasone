07ce57b14b245bf159ff77d081f6025bc7cc2579
nakasoneyoshihiro <nakasone@colrevo.co.jp>
2020-11-21 12:25:55 +0900

    Hello React Nakasone

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello <span class="fsBig">REACT NAKASONE</span>
         </p>
         <a
           className="App-link"
